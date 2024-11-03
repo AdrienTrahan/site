4e76c0325a44a5d14676d2a60e762a46807d4370
Adrien Trahan <adrientrahan@Adriens-MBP.ht.home>
2024-11-02 21:20:12 -0400

    Hover animation fixed

--- src/components/ContactMethod/index.svelte
+++ src/components/ContactMethod/index.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <a target="_blank" rel="noopener noreferrer" class="flex justify-center items-center group" href={link}>
-    <span class={`${ICON_COLORS[color]} h-0 flex justify-center items-center scale-125 group-hover:scale-150 md:scale-100 transition-all`}>
+    <span class={`${ICON_COLORS[color]} h-0 flex justify-center items-center scale-125 group-hover:scale-150 md:group-hover:scale-100 md:scale-100 transition-all`}>
         <svelte:component size="1.0x" this={icon} />
     </span>
     <p class={`${TEXT_COLORS[color]} max-w-0 md:max-w-24 -translate-x-8 opacity-0 md:translate-x-0 md:opacity-100 md:block font-medium ml-2 duration-300 transition-all group-hover:underline underline-offset-0 group-hover:underline-offset-4`}>{text}</p>
