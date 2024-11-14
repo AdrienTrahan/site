19af7def162f1e4dd045e3a64c5d6ddbe6db1ff5
Adrien Trahan <adrientrahan@Adriens-MBP.ht.home>
2024-11-13 20:53:45 -0500

    updated cv

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -4,6 +4,7 @@ import { API_URL } from "./consts";
 export async function getLatestUpdateTime(fetch: LoadEvent["fetch"]){
     return fetch(API_URL)
         .then(response => response.json())
+        .then(response => response.filter((commit: any) => !commit?.commit?.message?.toUpperCase().includes("CV")))
         .then(data => {
             if (data.length > 0) return new Date(data[0].commit.committer.date);
         })
